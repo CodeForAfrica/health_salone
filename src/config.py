7f2c03e35ebb68f521027e3d762e76e200703abd
Andrew Kamau <andkamau@gmail.com>
2016-06-10 18:15:45 +0300

    log location on config

--- src/config.py
+++ src/config.py
@@ -6,7 +6,8 @@ WEB_THREADS = 20
 
 LOGS = dict(
         WEB='twistd-web-service.log',
-        CONSUMER='/logs/consumer.log'
+        CONSUMER='/logs/consumer.log',
+        LOCATION='%s/health_salone/logs' % (HOME)
         )
 
 MESSAGE_BROKER = os.getenv('SL_RABBITMQ_URL')
